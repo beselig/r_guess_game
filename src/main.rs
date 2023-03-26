89fac63e9cd6408c1e49aa69e8a425ded7077771
Benno Selig <selig.benno@pm.me>
2023-03-26 17:46:27 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess:");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // the Err(_) matches against all Error values. _ is like a wildcard.
+            // We do not just check for Err, we "pattern match" against Err(_)
+            Err(_) => {
+                println!("Invalid number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
